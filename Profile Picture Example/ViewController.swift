66d030c81e7731798987e9f1bb6938dc7732ccb3
Dylan Jhaveri <dylanjhaveri@gmail.com>
2014-09-10 20:40:26 -0700

    load image async, off the main thread, then come back to the main thread for setting the UIImage

--- Profile Picture Example/ViewController.swift
+++ Profile Picture Example/ViewController.swift
@@ -15,8 +15,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let myProfilePictureUrl = NSURL(string: "http://graph.facebook.com/bobdylan/picture?type=square&width=640&height=640")
-        let imageData = NSData(contentsOfURL: myProfilePictureUrl)
-        self.imageView.image = UIImage(data: imageData)
+        
+        let activityIndictorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+        activityIndictorView.center = self.view.center
+        self.view.addSubview(activityIndictorView)
+        activityIndictorView.startAnimating()
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            // Do some async stuff
+            let imageData = NSData(contentsOfURL: myProfilePictureUrl)
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+                // Do some main thread stuff
+                self.imageView.image = UIImage(data: imageData)
+                activityIndictorView.stopAnimating()
+                
+                UIView.animateWithDuration(1.0, animations: {
+                    self.imageView.alpha = 1.0
+                    }, completion: {
+                        (value: Bool) in
+                        println("Animation complete!")
+                })
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
